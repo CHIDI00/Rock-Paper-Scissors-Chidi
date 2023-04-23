7bdecb272225cb6e0abc206a9c3331ef0b7c24e6
CHIDI MATHAIAS <chidimathayas@gmail.com>
2023-04-23 01:00:34 +0100

    Third commit

--- js/RPS.js
+++ js/RPS.js
@@ -6,7 +6,7 @@ const paper = document.getElementById('paper');
 const scissors = document.getElementById('scissors');
 const displayer = document.getElementById('display-win');
 const computer = document.querySelector('.computer');
-console.log(computer);
+const resetBtn = document.getElementById('resetBtn');
 
 
 const guesses = ['rock', 'paper', 'scissors'];
@@ -17,7 +17,7 @@ let compChoice = ''
 
 const Choice = function(yourChoice) {
     let compChoice = guesses[Math.floor(Math.random() * guesses.length)];
-    computer.classList = "fa-regular fa-hand-" + compChoice
+    computer.className = "computer fa-regular fa-hand-" + compChoice
     checkWin(yourChoice, compChoice)
 }
 // Choice()
@@ -39,7 +39,8 @@ const checkWin = function (yourChoice, compChoice) {
     }
     scoreCount(userScore, compScore);
     checkHigherScore(userScore, compScore)
-    computerCss(computer)
+    // computerCss(computer)
+    // updateWinner(userScore, compScore)
 }
 
 // Display if win or loss 
@@ -52,6 +53,7 @@ const scoreCount = function (userScore, compScore) {
     computerScore.innerHTML = compScore;
 }
 
+// Update on high score
 const checkHigherScore = function (userScore, compScore) {
     if (userScore > compScore) {
         playersScore.style.color = 'green';
@@ -65,6 +67,44 @@ const checkHigherScore = function (userScore, compScore) {
     }
 }
 
-const computerCss = function(computer) {
-    computer.style.fontSize = '20px'
+// Update winner 
+// const updateWinner = function(userScore, compScore) {
+//     if (userScore >= 3) {
+//         displayResult('You won the game 🎉!')
+//         checkWin(userScore, compScore) = false
+//     } else if (compScore >= 3){
+//         displayResult('You lost the game 😪')
+//     }
+
+// }
+// updateWinner(userScore, compScore)
+
+
+// Reset Game
+const reset = function() {
+    playersScore.innerHTML= userScore = 0;
+    computerScore.innerHTML = compScore = 0;
+
+    if (userScore === compScore) {
+        playersScore.style.color = '#000';
+        computerScore.style.color = '#000';
+    }
+
+    displayResult('Play again!');
+    displayer.style.color = 'blueviolet';
+
 }
+// reset()
+
+
+resetBtn.addEventListener('click', reset);
+
+// function factorial(n) {
+//     if (n === 0) {
+//         return 1
+//     } else {
+//         return n * factorial(n - 1)
+//     }
+// }
+// const result = factorial(8)
+// console.log(result);
